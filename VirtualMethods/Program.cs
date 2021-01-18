80c7e5a2d884e408a69c49dd3940ef080fe04013
henimex <49072861+henimex@users.noreply.github.com>
2021-01-19 02:12:39 +0300

    Abstract Class - Virtual+Interface

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SQLSERVER sql = new SQLSERVER();
+            MYSQLSERVER my = new MYSQLSERVER();
+
+            sql.Add("test server");
+            my.Add("test2 server");
+            sql.Delete();
+            my.Delete();
+        }
+    }
+
+    class Database
+    {
+        
+        public virtual void Add(string dbname)
+        {
+            //ezilebilmesini saglayan virtual olmasıdır
+            Console.WriteLine("added " + dbname);
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("deleted");
+        }
+    }
+
+    class SQLSERVER:Database
+    {
+        public override void Add(string dbname)
+        {
+            dbname = "SQLSERVER";
+            Console.WriteLine("Önce Değer verildi yada düzenleme yapıldı sonradan standart kod cagrıldı");
+            base.Add(dbname);
+        }
+    }
+
+    class MYSQLSERVER:Database
+    {
+
+    }
+}
