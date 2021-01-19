a3c85f276c4e71d0eee8de65453af5c10551aa1b
henimex <49072861+henimex@users.noreply.github.com>
2021-01-19 23:56:27 +0300

    RecapDemo

--- AbstractClasses/Program.cs
+++ AbstractClasses/Program.cs
@@ -6,9 +6,41 @@ namespace AbstractClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Database db = new Mysql();
+            db.Add();
+            db.Delete();
+
+            Database db2 = new SqlServerDB();
+            db2.Add();
+            db2.Delete();
+        }
+    }
+
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Standart ekleme komutları ile Eklendi");
+        }
+
+        public abstract void Delete();
+    }
+
+    class Mysql : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by mysql");
         }
     }
 
 
+    class SqlServerDB : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by sql");
+        }
+    }
 }
---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager cm = new CustomerManager();
+            cm.Logger = new DatabaseLogger();
+            //              FileLogger();
+            //              SmsLogger
+
+            cm.Add();
+        }
+    }
+
+    class CustomerManager
+    {
+        public IILogger Logger { get; set; }
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+            Logger.Log();
+        }
+    }
+
+    class DatabaseLogger : IILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Database");
+        }
+    }
+
+    class FileLogger : IILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged To File");
+        }
+    }
+
+    class SmsLogger : IILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged To SMS");
+        }
+    }
+
+    interface IILogger
+    {
+        void Log();
+    }
+
+}
