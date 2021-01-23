bd02b55f0b7ddc7f15bf26b4128b3c3280d59177
henimex-Dell <dev_henimex@hotmail.com>
2021-01-23 10:06:29 +0300

    BTK 19.4

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = new string[2]{ "Ferhat","Esila"};
+            Console.WriteLine(cities[0]);
+            cities = new string[3];
+            Console.WriteLine(cities[0]);
+            Console.ReadLine();
+
+            ArrayList newCities = new ArrayList();
+            newCities.Add("Akıllı Ali");
+            newCities.Add("Dr.Esila");
+            newCities.Add("Dilek");
+
+            Console.WriteLine(newCities[0]);
+
+            foreach (var city in newCities)
+            {
+                Console.WriteLine(city);
+            }
+
+            newCities.Add("Ferhat");
+
+            Console.WriteLine("Son Eleman : {0}",newCities[newCities.Count-1]);
+
+            newCities.Add(1);
+            newCities.Add(true);
+            newCities.Add('A');
+
+            foreach (var city in newCities)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
