ab3cd4addf43f00f693ff4647fb475c203cac145
henimex-Dell <dev_henimex@hotmail.com>
2021-01-23 10:45:11 +0300

    Generics

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -11,16 +11,18 @@ namespace Koleksiyonlar
     {
         static void Main(string[] args)
         {
-            string[] cities = new string[2]{ "Ferhat","Esila"};
+            string[] cities = new string[2] { "Ferhat", "Esila" };
             Console.WriteLine(cities[0]);
             cities = new string[3];
             Console.WriteLine(cities[0]);
-            Console.ReadLine();
+           
 
             ArrayList newCities = new ArrayList();
             newCities.Add("Akıllı Ali");
             newCities.Add("Dr.Esila");
             newCities.Add("Dilek");
+
+           
 
             Console.WriteLine(newCities[0]);
 
@@ -31,7 +33,7 @@ namespace Koleksiyonlar
 
             newCities.Add("Ferhat");
 
-            Console.WriteLine("Son Eleman : {0}",newCities[newCities.Count-1]);
+            Console.WriteLine("Son Eleman : {0}", newCities[newCities.Count - 1]);
 
             newCities.Add(1);
             newCities.Add(true);
@@ -42,7 +44,74 @@ namespace Koleksiyonlar
                 Console.WriteLine(city);
             }
 
+            List<string> ogrenciler = new List<string>();
+            ogrenciler.Add("Ferhat");
+            ogrenciler.Add("Dilek");
+
+            foreach (var ogrenci in ogrenciler)
+            {
+                Console.WriteLine(ogrenci);
+            }
+
+            bool ogrenciVarmi = ogrenciler.Contains("Dilek");
+            Console.WriteLine("Ogrenci Varmı Sonucu : {0}", ogrenciVarmi);
+            if (ogrenciVarmi)
+            {
+                Console.WriteLine("Evet Ogrenci Var");
+            }
+            else
+            {
+                Console.WriteLine("Aradıgınız ogrenci listede bulunamadı");
+            }
+
+            
+
+            Customer customer = new Customer();
+            customer.FirstName = "Amazın";
+            customer.Id = 1;
+
+            Customer customer2 = new Customer()
+            {
+                Id = 2,
+                FirstName = "e-Bay"
+            };
+
+            List<Customer> customers = new List<Customer>();
+            customers.Add(customer);    
+            customers.Add(customer2);
+
+            foreach (var item in customers)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+
+            customers.AddRange(new Customer[3]
+            {
+                new Customer{Id = 5, FirstName= "Robot"},
+                new Customer{Id = 6, FirstName= "Prenses"},
+                new Customer{Id = 7, FirstName= "Süpürge"}
+            });
+
+            customers.Contains(customer2);
+
+            var index = customers.IndexOf(customer);
+            var lastIndex = customers.LastIndexOf(customer2);
+            customers.Insert(0, customer);
+            customers.RemoveAll(x => x.FirstName == "Robot");
+            Console.WriteLine(index);
+            Console.WriteLine(lastIndex);
             Console.ReadLine();
         }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+       //public string CustomerNames
+       // {
+       //     get { return FirstName; }
+       // }
     }
 }
