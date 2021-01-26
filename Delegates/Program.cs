d12b167bdc6292c5dd15c8a66cbe62abfaedc9c8
henimex <49072861+henimex@users.noreply.github.com>
2021-01-26 21:14:21 +0300

    Functions

---
+++ Delegates/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void MyDelegate();
+    public delegate void MyDelegate2(string text);
+    public delegate int MyDelegate3(int n1, int n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            Matematik matematik = new Matematik();
+            //customerManager.SendMessage();
+            //customerManager.ShowAlert();
+
+            MyDelegate myDelegate = customerManager.SendMessage;
+            myDelegate += customerManager.ShowAlert;
+
+            myDelegate -= customerManager.SendMessage;
+            myDelegate();
+
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2 += customerManager.ShowAlert2;
+
+            myDelegate2("Naber yavru");
+
+            MyDelegate3 myDelegate3 = matematik.Topla;
+            var sonuc = myDelegate3(5, 6);
+            Console.WriteLine(sonuc);
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void ShowAlert()
+        {
+            Console.WriteLine("Becarefull");
+        }
+
+        public void SendMessage2(string text)
+        {
+            Console.WriteLine("İKinci delege " + text);
+        }
+
+        public void ShowAlert2(string text)
+        {
+            Console.WriteLine("İkinci delege " + text);
+        }
+    }
+
+    public class Matematik
+    {
+        public int Topla(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
---
+++ FuncKullanimi/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncKullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Action ile Func arasındaki fark : Action değer döndürmez Func Değer Döndürür.
+
+            //Console.WriteLine(Topla(2,3));
+
+
+            //Kullanım bu asamada delegeler gibi
+
+            Func<int, int, int> add = Topla;
+
+            Func<int> randFunc = delegate ()
+            {
+                Random rnd = new Random();
+                return rnd.Next(1, 50);
+            };
+
+            Func<int> rndFunc2 = () => new Random().Next(1, 100);
+
+            Console.WriteLine(randFunc());
+            Console.WriteLine(rndFunc2());
+            //delegeleri yada func ları kullanırken parantez ile orneklemek gerek yoksa fonksiyonun yapısını cıktı verir.
+
+            //Console.WriteLine(add(2,3));
+
+            Console.ReadLine();
+        }
+
+        static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
